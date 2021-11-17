11c0afa27b1111767f52a3db2931005c2e08ed36
Linden Chiu <hchiu87@gmail.com>
2021-11-16 22:34:34 -0800

    refactor test

--- test_rng_api.py
+++ test_rng_api.py
@@ -3,6 +3,8 @@ from rng_api import RandomAPI
 
 class TestRandomAPI:
 
-    def get_mastermind_code_length(self):
+    rc = RandomAPI(7)
+
+    def test_get_mastermind_code_length(self):
         rc = RandomAPI(7)
         assert len(rc.get_mastermind_code()) == 4
