3ef4b5f8580a53ab9dabec3b5a3b3443aac9d6c7
Linden Chiu <hchiu87@gmail.com>
2021-11-21 20:24:32 -0800

    add images, library

---
+++ pokemon_graphics_mapper.py
@@ -0,0 +1,13 @@
+from PIL import Image
+
+class PokemonGraphicsMapper:
+    pokemon_map = {
+       "0": './images'
+
+
+
+
+    }
+
+    def output_images(char_str):
+        image_keys = char_str.split()
